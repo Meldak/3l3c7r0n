22c765a75ce9144dc45743cc2823966bae19322d
Meldak <meldak.@gmail.com>
2018-04-18 16:43:36 -0500

    drag&drop

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const remote = require('electron').remote
-
-
 const main = remote.require('./main.js')
 
-let button = document.createElement('button')
-button.textContent = "Open Window"
-document.body.appendChild(button)
+//let button = document.createElement('button')
+//button.textContent = "Open Window"
+//document.body.appendChild(button)
 
-button.addEventListener('click', () => {
-	main.openWindow()
-})
+//button.addEventListener('click', () => {
+//	main.openWindow()
+//})
+
+document getElementById('drag').ondragstart = (event) => {
+	event.preventDefault()
+	ipcRender.send('ondragstart','./item')
+}
